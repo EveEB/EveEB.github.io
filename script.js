2d4160ed8b3f490e2e2dbde7bb584d23eea98178
EveEB <104207900+EveEB@users.noreply.github.com>
2022-08-05 16:39:58 +0100

    update files

---
+++ script.js
@@ -0,0 +1,17 @@
+// Sticky navbar
+
+window.onscroll = function () {
+  myFunction();
+};
+
+const navbar = document.getElementById("topnav");
+
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
