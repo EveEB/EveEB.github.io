23663810e69aa67b943b8ce62fd6d31f90ff9622
EveEB <104207900+EveEB@users.noreply.github.com>
2022-08-12 17:20:21 +0100

    update links and navbar

--- script.js
+++ script.js
@@ -21,5 +21,5 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
 toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("selected");
+  navbarLinks.classList.toggle("active");
 });
